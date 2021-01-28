44d2a76f736253078335b45cfa36cdccd4045c48
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2021-01-28 22:08:34 +0200

    Install redux-form package. Upgrage login commonent with this package.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 const Login = (props) => {
+    const onSubmit = (formData) =>{
+        console.log(formData);
+    }
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
     )
 }
+const LoginForm = (props) => {
+    return (
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={"Login"} name={"login"} component={"input"} />
+                </div>
+                <div>
+                <Field name={"password"}  placeholder={"Password"} component={"input"} />
+                </div>
+                <div>
+                <Field type={"checkbox"} name={"rememberMe"} component={"input"} /> remember me
+                </div>
+                <div>
+                    <button>Sign in</button>
+                </div>
+            </form>
+    )
+}
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 export default Login;
